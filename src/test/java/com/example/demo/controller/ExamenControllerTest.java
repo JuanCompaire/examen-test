78218eb7a59257a3726e16a25d0701ffca4cf382
JuanCompaire <juancompaire2004@gmail.com>
2024-02-12 08:55:52 +0100

    avanza el examen
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

---
+++ src/test/java/com/example/demo/controller/ExamenControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.ExamenService;
+import org.mockito.Mock;
+
+public class ExamenControllerTest {
+
+    ExamenController controller;
+
+    @Mock
+    private ExamenService service;
+
+}
---
+++ src/test/java/com/example/demo/model/ExamenTest.java
@@ -0,0 +1,129 @@
+package com.example.demo.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamenTest {
+
+    Examen examen;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Date fecha = sdf.parse("2021-10-10");
+
+    ExamenTest() throws ParseException {
+    }
+
+    @Test
+    void tesGetIdWhenIsNotEmpty() throws ParseException {
+        Examen exam = new Examen(1, 10, 2, null);
+
+        exam.setFechaNacimiento(fecha);
+        assertEquals(1, exam.getId());
+    }
+
+    @Test
+    void testSetIdWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setId(2);
+        assertEquals(2, exam.getId());
+    }
+
+    @Test
+    void testSetIdWhenIsEmpty() {
+        Examen exam = new Examen(null, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setId(2);
+        assertEquals(2, exam.getId());
+    }
+
+    @Test
+    void testGetIdWhenIsEmpty() {
+        Examen exam = new Examen(null, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        assertNull(exam.getId());
+    }
+
+    @Test
+    void testGetDividendoWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        assertEquals(10, exam.getDividendo());
+    }
+
+    @Test
+    void testGetDividendoWhenIsEmpty() {
+        Examen exam = new Examen(1, null, 2, null);
+        exam.setFechaNacimiento(fecha);
+        assertNull(exam.getDividendo());
+    }
+
+    @Test
+    void testSetDividendoWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setDividendo(20);
+        assertEquals(20, exam.getDividendo());
+    }
+
+    @Test
+    void testSetDividendoWhenIsEmpty() {
+        Examen exam = new Examen(1, null, 2, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setDividendo(20);
+        assertEquals(20, exam.getDividendo());
+    }
+    @Test
+    void testGetDivisorWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        assertEquals(2, exam.getDivisor());
+    }
+
+    @Test
+    void testGetDivisorWhenIsEmpty() {
+        Examen exam = new Examen(1, 10, null, null);
+        exam.setFechaNacimiento(fecha);
+        assertNull(exam.getDivisor());
+    }
+
+    @Test
+    void testSetDivisorWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setDivisor(20);
+        assertEquals(20, exam.getDivisor());
+    }
+
+    @Test
+    void testSetDivisorWhenIsEmpty() {
+        Examen exam = new Examen(1, 10, null, null);
+        exam.setFechaNacimiento(fecha);
+        exam.setDivisor(20);
+        assertEquals(20, exam.getDivisor());
+    }
+
+    @Test
+    void testGetFechaNacimientoWhenIsNotEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        exam.setFechaNacimiento(fecha);
+        assertEquals(fecha, exam.getFechaNacimiento());
+    }
+
+    @Test
+    void testGetFechaNacimientoWhenIsEmpty() {
+        Examen exam = new Examen(1, 10, 2, null);
+        assertNull(exam.getFechaNacimiento());
+    }
+
+
+
+
+}
